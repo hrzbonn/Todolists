1e64d88d68640d55121492ec8592b6e0794137aa
piseyb <piseyb@uni-bonn.de>
2020-05-26 16:16:51 +0200

    Test Ilias Version 6.0

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.0.9";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "5.4.9";
+$ilias_max_version = "6.0.0";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Pierre Seyb";
