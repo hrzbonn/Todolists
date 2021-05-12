3a0b23878751680b1d9cce70bb6cab98e651c52f
piseyb <piseyb@uni-bonn.de>
2021-05-12 11:06:31 +0200

    Update to Version 6.9

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.0.9";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "6.6.0";
+$ilias_max_version = "6.9.0";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Vincent Wolf";
