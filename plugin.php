e67a769b7bba0166570d7426c863d98a3f5d734a
viwolf <viwolf@uni-bonn.de>
2021-05-27 12:42:49 +0200

    Version 1.0.10 - new responsibilities

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = "xtdo";
  
 // code version; must be changed for all code changes
-$version = "1.0.9";
+$version = "1.0.10";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
@@ -12,6 +12,6 @@ $ilias_min_version = "4.0.0";
 $ilias_max_version = "6.9.0";
  
 // optional, but useful: Add one or more responsible persons and a contact email
-$responsible = "Vincent Wolf";
-$responsible_mail = "viwolf@uni-bonn.de";
+$responsible = "Patrick Veith";
+$responsible_mail = "pveith@uni-bonn.de";
 ?>
