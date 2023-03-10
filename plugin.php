a90597f2d539ee5a5c7a90c5a89b5cab76566b3a
Mirco Hilbert <mhilbert@users.noreply.github.com>
2023-03-10 11:55:04 +0100

    ILIAS max version changed to 7.999

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "xtdo";
  
 // code version; must be changed for all code changes
-$version = "1.0.10";
+$version = "1.0.11";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.0.0";
-$ilias_max_version = "6.9.0";
+$ilias_max_version = "7.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Patrick Veith";
